0be33e8d43d4aa6d2dc137b73d171fd8484aa903
Jagan11 <35328309+Jagan11@users.noreply.github.com>
2025-08-10 16:07:57 +0800

    Update run_homebrew_install.sh

--- scripts/run_homebrew_install.sh
+++ scripts/run_homebrew_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "Starting Homebrew installation via wrapper script..."
 
-# Run the actual install script from the same directory
+echo "Running wrapper script to install Homebrew..."
+
 NONINTERACTIVE=1 /bin/bash "$(dirname "$0")/install_homebrew.sh"
 
 if [ $? -ne 0 ]; then
-  echo "Homebrew installation failed."
+  echo "Homebrew installation failed in wrapper!"
   exit 1
 fi
 
